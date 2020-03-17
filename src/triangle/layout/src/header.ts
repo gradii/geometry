b2e4e2b2c1af03f0c4ecfb8efa8a47adefd56a6f
LinBo Len <linbolen@gradii.com>
2020-03-17 22:21:46 +0800

    style(theme): layout add header light dark theme

--- src/triangle/layout/src/header.ts
+++ src/triangle/layout/src/header.ts
@@ -5,14 +5,20 @@
  * Use of this source code is governed by an MIT-style license
  */
 
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'tri-header',
   template: `
     <ng-content></ng-content>
-  `
+  `,
+  host    : {
+    '[class.tri-layout-light]': "theme === 'light'",
+    '[class.tri-layout-dark]' : "theme === 'dark'"
+  }
 })
 export class Header {
+  @Input() theme: 'light' | 'dark';
+
   @HostBinding('class.tri-layout-header') _layoutHeader = true;
 }
