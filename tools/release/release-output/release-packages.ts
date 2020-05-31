977977cd2fe432e928601252bbfbb5a594bb9797
LinBo Len <linbolen@gradii.com>
2020-05-31 15:49:24 +0800

    fix build script

--- tools/release/release-output/release-packages.ts
+++ tools/release/release-output/release-packages.ts
@@ -1,10 +1,4 @@
 /** Packages that will be published to NPM by the publish release task. */
 export const releasePackages = [
-  'cdk',
-  'material',
-  'google-maps',
-  'youtube-player',
-  'cdk-experimental',
-  'material-experimental',
-  'material-moment-adapter',
+  'triangle',
 ];
