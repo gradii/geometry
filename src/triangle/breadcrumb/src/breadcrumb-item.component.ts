65ae65ea48fa7209390945832ae9568892daaa09
LinBo Len <linbolen@gradii.com>
2021-04-28 20:33:35 +0800

    fix: breadcrumb

--- src/triangle/breadcrumb/src/breadcrumb-item.component.ts
+++ src/triangle/breadcrumb/src/breadcrumb-item.component.ts
@@ -4,18 +4,29 @@
  * Use of this source code is governed by an MIT-style license
  */
 
-import { Component, Host } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Host
+} from '@angular/core';
 import { BreadcrumbComponent } from './breadcrumb.component';
+import { BreadcrumbItemInterface } from './breadcrumbItemInterface';
 
 @Component({
-  selector: 'tri-breadcrumb-item',
-  template: `
+  selector       : 'tri-breadcrumb-item',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template       : `
     <span class="tri-breadcrumb-link">
       <ng-content></ng-content>
     </span>
-    <span class="tri-breadcrumb-separator">{{breadCrumbComponent?.separator}}</span>`
+    <span *ngIf="!isLast"
+          class="tri-breadcrumb-separator">{{breadCrumbComponent.separator}}</span>`
 })
-export class BreadcrumbItemComponent {
+export class BreadcrumbItemComponent implements BreadcrumbItemInterface {
+  get isLast() {
+    return this.breadCrumbComponent.breadCrumbItems.last === this;
+  }
+
   constructor(@Host() public breadCrumbComponent: BreadcrumbComponent) {
   }
 }
--- src/triangle/breadcrumb/src/breadcrumb.component.ts
+++ src/triangle/breadcrumb/src/breadcrumb.component.ts
@@ -4,25 +4,37 @@
  * Use of this source code is governed by an MIT-style license
  */
 
-import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ContentChildren,
+  Input,
+  QueryList,
+  ViewEncapsulation
+} from '@angular/core';
+import { BreadcrumbItemComponent } from './breadcrumb-item.component';
 
 @Component({
-  selector     : 'tri-breadcrumb',
-  encapsulation: ViewEncapsulation.None,
-  template     : `
+  selector       : 'tri-breadcrumb',
+  encapsulation  : ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template       : `
     <ng-content></ng-content>`,
-  styleUrls    : ['../style/breadcrumb.css'],
-  styles       : [`tri-breadcrumb {
-    display: block;
-  }`]
+  styleUrls      : ['../style/breadcrumb.css'],
+  host           : {
+    'class': 'tri-breadcrumb'
+  }
 })
 export class BreadcrumbComponent {
+
+  @ContentChildren(BreadcrumbItemComponent, {descendants: false})
+  breadCrumbItems: QueryList<BreadcrumbItemComponent>;
+
   /**
    * Separator
    * 分隔符自定义
    */
   @Input() separator = '/';
-  @HostBinding('class.tri-breadcrumb') _breadcrumb = true;
 
   constructor() {
   }
