1dc19a9424ca3f4083623f191299dac31e7ef5ff
LinBo Len <linbolen@gradii.com>
2021-05-06 22:24:34 +0800

    wip

--- src/triangle/tree-select/src/tree-select.component.ts
+++ src/triangle/tree-select/src/tree-select.component.ts
@@ -1,4 +1,10 @@
-import { Component } from "@angular/core";
+/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license
+ */
+
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'tri-tree-select',
--- src/triangle/tree-select/src/tree-select.module.ts
+++ src/triangle/tree-select/src/tree-select.module.ts
@@ -1,16 +1,33 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { TreeSelectComponent } from "./tree-select.component";
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TriCommonModule } from '@gradii/triangle/core';
+import { TriIconModule } from '@gradii/triangle/icon';
+import { OptionContainerComponent } from '@gradii/triangle/tree-select/src/option-container.component';
+import { SelectTopControlComponent } from '@gradii/triangle/tree-select/src/select-top-control.component';
+import { SelectUnselectableDirective } from '@gradii/triangle/tree-select/src/select-unselectable.directive';
+import { SelectComponent } from '@gradii/triangle/tree-select/src/select.component';
 
 @NgModule({
     imports: [
-        CommonModule,
+      CommonModule,
+
+      FormsModule,
+      OverlayModule,
+      TriIconModule,
+      OverlayModule,
+      TriCommonModule,
     ],
     declarations: [
-        TreeSelectComponent,
+      SelectComponent,
+      OptionContainerComponent,
+      SelectTopControlComponent,
+      SelectUnselectableDirective
     ],
     exports: [
-        TreeSelectComponent,
+      SelectComponent,
+      OptionContainerComponent,
     ]
 })
 export class TriTreeSelectModule {
