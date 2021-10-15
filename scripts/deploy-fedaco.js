11846f990c10c697ca9a030658a25ef844e4db91
LinBo Len <linbolen@gradii.com>
2021-10-15 08:44:10 +0800

    fedaco build

--- scripts/deploy-fedaco.js
+++ scripts/deploy-fedaco.js
@@ -9,10 +9,11 @@ const {exec, set, cd, cp, rm, chmod} = require('shelljs');
 const {join}                         = require('path');
 const {glob}                         = require("glob");
 const fs                             = require('fs');
-const fse                             = require('fs-extra');
+const fse                            = require('fs-extra');
 const format                         = require('date-fns/format');
 const tar                            = require('tar');
 const path                           = require('path');
+const prettier                       = require('prettier')
 
 // ShellJS should throw if any command fails.
 set('-e');
@@ -27,13 +28,15 @@ cd(projectDirPath);
 const bazelBinPath = exec(`yarn -s bazel info bazel-bin`).stdout.trim();
 
 /** Output path for the Bazel dev-app web package target. */
-const webPackagePath = join(bazelBinPath, 'src/dev-app/web_package');
+const webPackagePath = join(bazelBinPath, 'src/fedaco/npm_package');
 
 /** Destination path where the web package should be copied to. */
-const distPath = join(projectDirPath, 'dist/dev-app-web-pkg');
+const distPath = join(projectDirPath, 'dist/releases/fedaco');
+
+fse.ensureDirSync(distPath);
 
 // Build web package output.
-exec('yarn -s bazel build //src/dev-app:web_package');
+exec('yarn -s bazel build //src/fedaco:npm_package');
 
 // Clear previous deployment artifacts.
 rm('-Rf', distPath);
@@ -51,23 +54,43 @@ chmod('-R', 'u+w', distPath);
 // Run the Firebase CLI to deploy the hosting target.
 // exec(`yarn -s firebase deploy --only hosting`);
 
+
+//build devops
 async function build() {
   let files = glob.sync(`${distPath}/**/*.js`, {nodir: true});
   files.forEach(file => {
     try {
-      const content = fs.readFileSync(file, 'utf8').replace(/^\/\/# sourceMappingURL=.+?$/mg, '');
+      let content = fs.readFileSync(file, 'utf8')
+        .replace(/^\/\/# sourceMappingURL=.+?$/mg, '')
+        .replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '$1')
+
+      content = prettier.format(content, {
+        parser: 'espree',
+        trailingComma: "es5",
+        tabWidth: 2,
+        semi: false,
+        singleQuote: true,
+      })
+
       fs.writeFileSync(file, content, 'utf8');
     } catch (e) {
-      debugger
     }
   })
 
+  files = glob.sync(`${distPath}/**/*.map`, {nodir: true})
+
+  files.forEach(file => {
+    fs.unlinkSync(file);
+  })
+
+
   const dateString = format(new Date, 'yyyyMMdd_HHmmss')
+
 
   const fileMap = [
     {
-      key: 'dev-app-web-pkg',
-      cwd: './dist/dev-app-web-pkg',
+      key   : 'fedaco-npm-pkg',
+      cwd   : distPath,
       source: ['./'],
       output: `dist/build-tar/{key}_${dateString}.tgz`
     },
@@ -82,16 +105,15 @@ async function build() {
   fse.emptyDirSync(buildTarDir);
 
 
-//build devops
   for (const val of fileMap) {
     await tar.c(
       {
-        gzip: true,
-        file: val.output.replace('{key}', val.key),
+        gzip  : true,
+        file  : val.output.replace('{key}', val.key),
         prefix: val.key,
-        cwd: val.cwd,
+        cwd   : val.cwd,
       }, val.source
-    );
+    )
 
     console.log(`${val.key} .. tarball has been created ..`);
   }
