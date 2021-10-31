cf8179e02efead31b109d45505293583acbbb3a9
LinBo Len <linbolen@gradii.com>
2021-10-31 16:40:39 +0800

    fedaco build

--- tools/gulp/tasks/fedaco-gen-doc.ts
+++ tools/gulp/tasks/fedaco-gen-doc.ts
@@ -1,36 +1,41 @@
 import { dest, src, task } from 'gulp';
-import { buildConfig } from '../../package-tools';
-import { join } from 'path';
+import path, { join } from 'path';
 import * as through2 from 'through2';
 import * as ts from 'typescript';
+import VinylFile from 'vinyl';
+import { buildConfig } from '../../package-tools';
 import { refactorGenerateMd } from './generate-md/refactor-generate-md';
 
 task('fedaco-gen-doc:dist', async () => {
   console.log(buildConfig.projectDir);
 
   const projectDir = buildConfig.projectDir;
-  const outputDir = join(buildConfig.outputDir, 'output-refactor-ts');
+  const outputDir  = join(buildConfig.outputDir, 'output-generate-md');
 
   src('src/fedaco/test/**/fedaco-integration.spec.ts', {
     base: 'src/fedaco/test/'
   })
     .pipe(
-      through2.obj((file, _, cb) => {
+      through2.obj(function (file, _, cb) {
+        const base = path.join(file.path, '..');
         if (file.isBuffer()) {
 
           const filename = 'test.ts';
-          const code = file.contents.toString();
+          const code     = file.contents.toString();
 
           const sourceFile = ts.createSourceFile(
             filename, code, ts.ScriptTarget.Latest
           );
 
+          const generateContext = {};
+
           const transformationResult = ts.transform(sourceFile, [
-            refactorGenerateMd(),
+            refactorGenerateMd(generateContext),
           ]);
+          console.log(generateContext);
 
           const transformedSourceFile = transformationResult.transformed[0];
-          const printer = ts.createPrinter();
+          const printer               = ts.createPrinter();
 
           const result = printer.printNode(
             ts.EmitHint.Unspecified,
@@ -38,9 +43,17 @@ task('fedaco-gen-doc:dist', async () => {
             transformedSourceFile
           );
 
-          file.contents = Buffer.from(result);
+          this.push(
+            new VinylFile(
+              {
+                base    : base,
+                path    : path.join(base, 'first.txt'),
+                contents: Buffer.from(result)
+              }
+            )
+          );
         }
-        cb(null, file);
+        cb();
       }))
     .pipe(
       dest(outputDir)
--- tools/gulp/tasks/generate-md/refactor-generate-md.ts
+++ tools/gulp/tasks/generate-md/refactor-generate-md.ts
@@ -1,9 +1,11 @@
 import * as ts from 'typescript';
 
 
-export function refactorGenerateMd<T extends ts.Node>(/*typeChecker: TypeChecker*/): ts.TransformerFactory<T> {
+export function refactorGenerateMd<T extends ts.Node>(/*typeChecker: TypeChecker*/
+                                                      generateContext: any): ts.TransformerFactory<T> {
+  generateContext.files = [];
+
   return (context) => {
-    let parent;
     const visit: ts.Visitor = (node) => {
       if (ts.isDecorator(node)) {
         return undefined;
@@ -14,19 +16,14 @@ export function refactorGenerateMd<T extends ts.Node>(/*typeChecker: TypeChecker
       }
 
       if (
-        ts.isCallExpression(node) &&
-        ts.isIdentifier(node.expression) &&
-        node.expression.text === 'array_merge'
+        ts.isExpressionStatement(node) &&
+        ts.isCallExpression(node.expression) &&
+        node.expression.arguments.length === 2 &&
+        ts.isStringLiteral(node.expression.arguments[0])
       ) {
-
-        return ts.createArrayLiteral(
-          node.arguments.map(it => {
-            return ts.createSpread(
-              ts.isConditionalExpression(it) ? ts.createParen(it) : it
-            );
-          }),
-          node.arguments.length > 2
-        );
+        generateContext.files.push({
+          fileName: node.expression.arguments[0].text
+        });
       }
 
       return ts.visitEachChild(node, (child) => visit(child), context);
