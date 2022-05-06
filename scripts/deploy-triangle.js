2836a5cb6261d718124ee82785faeef669e38e3e
LinBo Len <linbolen@gradii.com>
2022-05-06 12:44:30 +0800

    feat: update triangle version

--- scripts/deploy-triangle.js
+++ scripts/deploy-triangle.js
@@ -7,13 +7,14 @@
 
 const {exec, set, cd, cp, rm, chmod} = require('shelljs');
 const {join}                         = require('path');
-const {glob}                         = require("glob");
+const glob                           = require("globby");
 const fs                             = require('fs');
 const fse                            = require('fs-extra');
 const format                         = require('date-fns/format');
 const tar                            = require('tar');
 const path                           = require('path');
 const prettier                       = require('prettier')
+const buildConfig                    = require('../build-config');
 
 // ShellJS should throw if any command fails.
 set('-e');
@@ -24,56 +25,49 @@ const projectDirPath = join(__dirname, '../');
 // Go to project directory.
 cd(projectDirPath);
 
-/** Path to the bazel-bin directory. */
-const bazelBinPath = exec(`yarn -s bazel info bazel-bin`).stdout.trim();
-
-/** Output path for the Bazel dev-app web package target. */
-const webPackagePath = join(bazelBinPath, 'src/triangle/npm_package');
-
 /** Destination path where the web package should be copied to. */
-const distPath = join(projectDirPath, 'dist/releases/triangle');
-
-fse.ensureDirSync(distPath);
+const distPath = join(projectDirPath, 'dist/libs/triangle');
 
 // Build web package output.
-exec('yarn -s bazel build //src/triangle:npm_package --config=snapshot');
-
-// Clear previous deployment artifacts.
-rm('-Rf', distPath);
-
-// Copy the web package from the bazel-bin directory to the project dist
-// path. This is necessary because the Firebase CLI does not support deployment
-// of a public folder outside of the "firebase.json" file.
-cp('-R', webPackagePath, distPath);
-
-// Bazel by default marks output files as `readonly` to ensure hermeticity. Since we moved
-// these files out of the `bazel-bin` directory, we should make them writable. This is necessary
-// so that subsequent runs of this script can delete old contents from the deployment dist folder.
-chmod('-R', 'u+w', distPath);
-
-// Run the Firebase CLI to deploy the hosting target.
-// exec(`yarn -s firebase deploy --only hosting`);
-
+exec('yarn -s nx build triangle --prod');
 
 //build devops
 async function build() {
-  let files = glob.sync(`${distPath}/**/*.js`, {nodir: true});
+  const pkg     = fs.readFileSync(`${distPath}/package.json`, 'utf8');
+  const pkgJson = JSON.parse(pkg);
+  ['peerDependencies', 'dependencies', 'devDependencies'].forEach(dep => {
+    if (pkgJson[dep]) {
+      Object.keys(pkgJson[dep]).forEach(it => {
+        if (it.startsWith('@angular')) {
+          delete pkgJson[dep][it];
+        }
+      })
+    }
+  });
+
+  fs.writeFileSync(`${distPath}/package.json`, JSON.stringify(pkgJson, null, 2));
+
+  let files = glob.sync([`${distPath}/**/*.d.ts`, `${distPath}/**/*.js`], {nodir: true});
   files.forEach(file => {
     try {
       let content = fs.readFileSync(file, 'utf8')
         .replace(/^\/\/# sourceMappingURL=.+?$/mg, '')
         .replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '$1')
 
+      const licenseBanner = buildConfig.fedacoLicenseBanner;
+      const isJs = file.endsWith('.js');
       content = prettier.format(content, {
-        parser: 'espree',
-        trailingComma: "es5",
-        tabWidth: 2,
-        semi: false,
-        singleQuote: true,
-      })
+        parser       : isJs ? 'espree' : 'typescript',
+        trailingComma: isJs ? "es5" : "none",
+        tabWidth     : isJs ? 2 : 4,
+        semi         : isJs ? false : true,
+        singleQuote  : true,
+      });
 
-      fs.writeFileSync(file, content, 'utf8');
+      fs.writeFileSync(file, `${licenseBanner}
+${content}`, {encoding: 'utf8', mode: 10644});
     } catch (e) {
+      console.error(e);
     }
   })
 
@@ -117,6 +111,9 @@ async function build() {
 
     console.log(`${val.key} .. tarball has been created ..`);
   }
+
+
+  exec(`yarn publish --registry=https://npm.pkg.github.com/ ${distPath}`);
 }
 
 try {
